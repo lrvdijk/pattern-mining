7bee2584f12fdc9927525228c0102743401b3417
Lucas van Dijk <info@return1.net>
2016-01-03 16:46:21 +0100

    Create association rules module
    This module contains code which generates association rules in the
    form of A => B based on frequent patterns

---
+++ dbdm/rules.py
@@ -0,0 +1,38 @@
+import itertools
+
+from dbdm import fp_growth, measures, itemsets
+
+
+def generate_association_rules(df, min_support=10, min_conf=70):
+    min_support_count = int((len(df) * min_support) / 100)
+
+    iter = fp_growth.find_frequent_itemsets(itemsets.generator(df),
+                                            min_support_count,
+                                            include_support=True)
+    for itemset in iter:
+        for rule in generate_rules(df, itemset, min_conf):
+            yield rule
+
+
+def non_empty_subsets(iterable):
+    s = list(iterable)
+    return itertools.chain.from_iterable(
+        itertools.combinations(s, r) for r in range(1, len(s))
+    )
+
+
+def generate_rules(df, pattern, min_conf=70):
+    itemtuple, support = pattern
+
+    if len(itemtuple) < 2:
+        return
+
+    itemset = set(itemtuple)
+    for subset in non_empty_subsets(itemset):
+        subset = set(subset)
+
+        b = itemset - subset
+        confidence = measures.confidence(df, subset, b, {'ab': support}) * 100
+
+        if confidence >= min_conf:
+            yield (subset, b)
