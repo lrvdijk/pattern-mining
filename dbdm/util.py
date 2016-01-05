3c9da3bc4bea8a2967c2934a35e4c2173bdc6d9a
Lucas van Dijk <info@return1.net>
2016-01-05 11:43:04 +0100

    Add function to nicely format an association rule

--- dbdm/util.py
+++ dbdm/util.py
@@ -1,4 +1,20 @@
+import pandas as pd
+import numpy as np
+
+NUMERIC_DATATYPES = [
+    'int16', 'int32', 'int64', 'float16', 'float32', 'float64'
+]
+
+
 def generate_criteria(df, values):
+    """
+    :param df:
+    :type df: pd.DataFrame
+    :param values:
+    :type values:
+    :return:
+    :rtype:
+    """
     criteria = None
     for item in values:
         if criteria is None:
@@ -6,8 +22,18 @@ def generate_criteria(df, values):
         else:
             criteria &= df[item[1]] == item[0]
 
+        if df.dtypes[item[1]] in NUMERIC_DATATYPES:
+            criteria |= df[item[1]].apply(np.isnan)
+
     return criteria
 
 
 def create_subdataframe(df, values):
     return df[generate_criteria(df, values)]
+
+
+def format_rule(a, b):
+    return "{{{a}}} => {{{b}}}".format(
+        a=", ".join("{1}: {0}".format(*v) for v in a),
+        b=", ".join("{1}: {0}".format(*v) for v in b)
+    )
